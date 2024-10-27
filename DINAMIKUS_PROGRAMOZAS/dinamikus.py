b28017b4a06958778e84b2213fe7d51456a5d854
dekany01 <163033866+dekany01@users.noreply.github.com>
2024-10-27 11:30:37 +0100

    Update dinamikus.py

--- DINAMIKUS_PROGRAMOZAS/dinamikus.py
+++ DINAMIKUS_PROGRAMOZAS/dinamikus.py
@@ -16,20 +16,29 @@ import sys
 #  3. INTEGER n
 #
 
-sys.set_int_max_str_digits(1000000)
-
+sys.set_int_max_str_digits(1000000)  # Megnöveljük a megjeleníthető számjegyek számát a magas függvényparaméterek miatt.
 def fibonacciModified(t1, t2, n):
-    # Write your code here
-    fibArr = [0,t1,t2]
+    
+    fibList = [0] * (n + 1) # Létrehozunk egy 'n+1' elemű listát (az egyszerűbb indexelés miatt).
 
-    i = 3
-    while i <= n:
+    # A sorozat megadott két első értéke a lista 1. és 2. indexű eleme lesz:
+    
+    fibList[1] = t1
+    fibList[2] = t2
+    
+    # Az összes többi elemet szintén eltároljuk a listában (alulról felülre építkezve), hiszen az 'n'-nél kisebb indexű elemeket is többször felhasználjuk.
+   
+    i = 3 
+    while i <= n: # 3. indextől kezdve nézzük a sorozat többi elemét.
 
-        newNumber = [(fibArr[i-2] + fibArr[i-1]**2)]
-        fibArr += newNumber
-        i += 1
+        newNumber = fibList[i-2] + fibList[i-1]**2 # Kiszámoljuk a sorozat új elemét a feladatban megadott formula alapján.
+        fibList[i] = newNumber # A lista i. indexű eleme a sorozat i. eleme lesz.
         
-    return fibArr[n]
+        i += 1 
+
+    # Így kapunk egy 1 dimenziós táblázatot, melynek adott 'i.' indexű eleme a módosított Fibonacci sorozat 'i'. eleme lesz.
+    
+    return fibList[n] # A függvény visszaadja a sorozat n. elemét.
     
 
 if __name__ == '__main__':
