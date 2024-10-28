dfb593093b8a3363f9a68f2360479c6760208108
dekany01 <163033866+dekany01@users.noreply.github.com>
2024-10-28 19:39:16 +0100

    Update kereses.py

--- KERESES/kereses.py
+++ KERESES/kereses.py
@@ -17,21 +17,24 @@ import sys
 
 def pairs(k, arr):
     # Write your code here
-    arr.sort()
+    arr.sort()  # Rendezzük növekvő számsorrendbe a lista elemeit.
     
-    sumDiff = 0
+    sumDiff = 0 # Az összes kritériumnak megfelelő párt számláló változó.
     
-    for i in range(len(arr)):
+    for i in range(len(arr)): # Végig iterálunk a lista elemein.
         
-        for j in range(i, len(arr)):
+        for j in range(i, len(arr)): # Megnézzük az adott elemtől jobbra lévő (tehát nagyobb) számokat a listában.
             
-            if arr[j] == arr[i] + k:
-                sumDiff += 1
-                break
-            elif arr[j] > arr[i] + k:
+            if arr[j] == arr[i] + k:  # Ha az i. indexű elemtől jobbra lévő aktuális egész szám akkora, mint az i. indexű elem + a célzott különbség,
+                                      # akkor megtaláltuk az i. indexű elem párját.
+
+                sumDiff += 1            # Hozzáadunk a számlálóhoz 1-et, majd
+                break                   # kilépünk a belső ciklusból.
+           
+            elif arr[j] > arr[i] + k:    # Ha az i. indexű elemtől jobbra lévő aktuális egész szám nagyobb, mint a különbség + az i. indexű elem, akkor nincs értelme tovább keresni az i. indexű elem párját.
                 break
     
-    return sumDiff
+    return sumDiff         # A végén visszaadja a függvény a talált párok számát.
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
