c858c48a81031bdd63c0a73fd0d8bb80098d37e2
dekany01 <163033866+dekany01@users.noreply.github.com>
2024-10-28 19:08:34 +0100

    Update moho.py

--- MOHO_STRATEGIA/moho.py
+++ MOHO_STRATEGIA/moho.py
@@ -18,14 +18,24 @@ import sys
 def maxMin(k, arr):
     # Write your code here
     
-    minUnfair = 10**9
-    arr.sort()
-    for i in range(len(arr)-k+1):
+    minUnfair = 10**9  # Ebben a változóban tároljuk el a legkisebb értéket, amit a függvény később visszaad.
+                       # Miután a megengedett legkisebb szám 0, a legnagyobb pedig 10^9 lehet, 
+                       # ezért a várható legkisebb érték 10^9-nél nem lehet nagyobb.
+            
+    
+    arr.sort()         # Szám szerint növekvő sorrendbe tesszük a lista elemeit.
+                       # Ezáltal a nagyságrendileg hasonló méretű számok egymáshoz közelebb lesznek.
+    
+    for i in range(len(arr)-k+1):    # A rendezett lista elemeit végig iteráljuk úgy, hogy a lista utolsó elemétől visszaszámolunk 'k+1' darabot.
 
-        minArr = arr[i+k-1] - arr[i]
-
-        if minArr < minUnfair:
-            minUnfair = minArr
+        minArr = arr[i+k-1] - arr[i]  # Felesleges résztömbökre (részlistákra python esetében) bontanunk a teljes tömböt, hiszen
+                                      # a rendezett listában egy adott elemtől jobbra lévő számok mindig nagyobbak,
+                                      # ezért az aktuális elemet tekinthetjük minimumnak és az attól jobbra 'k-1' távolságra lévő elemet a maximumnak.
+                                      # ('k-1', mert egy k elemszámú tömb első eleme a 0. indexű, akkor a k-1. az utolsó)
+                                      # A fentiek miatt is kell csak a lista utolsó elemétől visszaszámolni k+1 távolságot.
+        
+        if minArr < minUnfair:        # Megnézzük, hogy az aktuálisan vizsgált elem kisebb-e, mint az eddigi legkisebb "igazságtalansági" érték.
+            minUnfair = minArr        # Ha igen, akkor az lesz az új legkisebb "igazságtalansági" érték.
             
     return minUnfair
 
